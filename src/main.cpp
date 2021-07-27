7e9dbf13922a8ed79608ea03895c3e97a5563bf4
Piotr Jan Paturej <p.paturej@stud.elka.pw.edu.pl>
2021-07-27 16:40:06 +0200

    Add multiplication and gradient operation showcase

--- src/main.cpp
+++ src/main.cpp
@@ -4,33 +4,12 @@
 
 int main()
 {
-    // CONST
-    // CONST
-    // CONST
-    // CONST
+    Matrix<float> A(4, 4, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
+    Matrix<float> B(4, 4, {17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32});
 
-    Matrix<float> A(3, 2, {1, 2, 3, 4, 5, 6});
-    // Matrix<float> B(2, 1, {1, 2});
-    // Matrix<float> C(1, 1, {1});
-    Matrix<float> D(2, 4, {1, 2, 3, 4, 5, 6, 7, 8});
+    (A * B).print_matrix();
+    Matrix<float>::compute_grad(A, B, B).print_matrix();
+    Matrix<float>::compute_grad(A, B, A).print_matrix();
 
-    // (A * D).print_matrix();
-    Matrix<float>::compute_grad(A, D, A).print_matrix();
-
-    // Matrix<float> A(1, 3, {1, 2.6, 3});
-    // Matrix<float> B(3, 3, {1, 2, 3, 4, 5, 6, 7, 8, 9});
-
-    // Matrix<float> A(4, 4, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16});
-    // Matrix<float> B(4, 4, {17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32});
-    // A.print_matrix();
-    // std::cout << std::endl;
-    // B.print_matrix();
-    // Matrix<float> C = A * B;
-
-    // Matrix<float> D = A * Matrix<float>(2, 2, {1, 0, 0, 0});
-    // Matrix<float> E = Matrix<float>(1, 2, {5, 0}) * B;
-    // E.print_matrix();
-    // std::cout << (C == A) << std::endl;
-    // std::cout << (C == A * B) << std::endl;
     return 0;
 }
